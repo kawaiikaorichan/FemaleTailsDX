7291803a60a07f3e566ed3811c46335ac3fedc62
Kaori <super_so_nic1@hotmail.com>
2021-11-30 22:54:52 -0600

    *Updating the GUI. *Adding textures for new models

--- FemaleTailsDX/OnnaTails.cpp
+++ FemaleTailsDX/OnnaTails.cpp
@@ -3,21 +3,21 @@
 #define ReplacePVM(a, b) helperFunctions.ReplaceFile("system\\" a ".PVM", "system\\" b ".PVM");
 NJS_MATERIAL* TemporaryMaterialArray[] = { nullptr };
 
-static Trampoline* LoadRegObjTextures_t = nullptr;
-static Trampoline* FreeRegObjTexlists_t = nullptr;
+Trampoline* LoadRegObjTextures_t = nullptr;
+Trampoline* FreeRegObjTexlists_t = nullptr;
 
-static NJS_TEXNAME GUI_TEXNAME[2] = {};
-static NJS_TEXLIST GUI_TEXLIST = { arrayptrandlengthT(GUI_TEXNAME, int) };
+static NJS_TEXNAME MILLIEGUI_TEXNAME[2] = {};
+static NJS_TEXLIST MILLIEGUI_TEXLIST = { arrayptrandlengthT(MILLIEGUI_TEXNAME, int) };
 
 static void __cdecl LoadRegObjTextures_r(int a1)
 {
-	LoadPVM("GUI", &GUI_TEXLIST);
+	LoadPVM("MILLIEGUI", &MILLIEGUI_TEXLIST);
 	((decltype(LoadRegObjTextures_r)*)LoadRegObjTextures_t->Target())(a1);
 }
 
 static void __cdecl FreeRegObjTexlists_r()
 {
-	njReleaseTexture(&GUI_TEXLIST);
+	njReleaseTexture(&MILLIEGUI_TEXLIST);
 	((decltype(FreeRegObjTexlists_r)*)FreeRegObjTexlists_t->Target())();
 }
 
@@ -32,7 +32,10 @@ extern "C"
 	__declspec(dllexport) void __cdecl Init(const char* path, const HelperFunctions& helperFunctions)
 	{
 		Icons_Init();
-		ReplacePVM("Miles", "OnnaTails")
+		ReplacePVM("Miles", "Millie");
+		ReplacePVM("m_head_1", "ProtoHead");
+		ReplacePVM("m_tr_p", "ProtoTornado");
+		ReplacePVM("ev_s_t2c_body", "SonicEVProtoTornado");
 	}
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
 }
